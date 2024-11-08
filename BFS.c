d53ba41739b307af9a69db7755829d4188396353
SudevOP1 <168339977+SudevOP1@users.noreply.github.com>
2024-11-08 10:49:05 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -5,26 +5,21 @@
 int adjacency_matrix[10][10], visited[10], q[10];
 int num_of_vertices, front = 0, rear = -1;
 
-void bfs(int vertex)
-{
+void bfs(int vertex) {
     int i;
-    for (i = 0; i < num_of_vertices; i++)
-    {
-        if (adjacency_matrix[vertex][i] == 1 && visited[i] == 0)
-        {
+    for (i = 0; i < num_of_vertices; i++) {
+        if (adjacency_matrix[vertex][i] == 1 && visited[i] == 0) {
             q[++rear] = i;
             visited[i] = 1;
             printf("%d ", i);
         }
     }
-    if (front <= rear)
-    {
+    if (front <= rear) {
         bfs(q[front++]);
     }
 }
 
-int main()
-{
+int main() {
     // clrscr();
 
     int i, j, vertex;
@@ -33,16 +28,13 @@ int main()
     scanf("%d", &num_of_vertices);
 
     printf("Enter adjacency matrix of the graph: ");
-    for (i = 0; i < num_of_vertices; i++)
-    {
-        for (j = 0; j < num_of_vertices; j++)
-        {
+    for(i = 0; i < num_of_vertices; i++) {
+        for(j = 0; j < num_of_vertices; j++) {
             scanf("%d", &adjacency_matrix[i][j]);
         }
     }
 
-    for (i = 0; i < num_of_vertices; i++)
-    {
+    for(i = 0; i < num_of_vertices; i++) {
         visited[i] = 0;
     }
 
